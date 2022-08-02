cdde52810b1c5e507d5d4cef186bb6fc904038ad
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:09:50 -0700

    updated Contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Contact = () => {
+    return (
+        <Container>
+            <div>THIS IS THE CONTACT PAGE</div>
+        </Container>
+        )
+};
+
+export default Contact;
