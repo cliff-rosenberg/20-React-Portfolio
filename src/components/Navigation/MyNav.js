5a1793b742b2770df66562ed83e1f7a97593a6ca
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:10:16 -0700

    updated Nav bar

--- src/components/Navigation/MyNav.js
+++ src/components/Navigation/MyNav.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import './MyNav.css'
 import { MDBIcon } from 'mdb-react-ui-kit';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
-function MyNav() {
+const MyNav = () => {
   return (
     <Navbar bg="dark" variant="dark">
-        <Container>
-        <MDBIcon className="mdb-icon text-info" fab icon="react" size='3x'/>
-          <Navbar.Brand href="#home">Cliff Rosenberg</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#protfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#resume">My Resume</Nav.Link>
-          </Nav>
-        </Container>
+          <MDBIcon className="mdb-icon text-info" fab icon="react" size='3x'/>
+          <LinkContainer to="/">
+            <Navbar.Brand href="#home">Cliff Rosenberg</Navbar.Brand>
+          </LinkContainer>
+            <Nav className="me-auto">
+              <LinkContainer to="/about">
+                <Nav.Link href="#about">About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/contact">
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/portfolio">
+                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/resume">
+                <Nav.Link href="#resume">My Resume</Nav.Link>
+              </LinkContainer>
+            </Nav>
       </Navbar>
   );
 }
 
-export { MyNav };
+export default MyNav;
