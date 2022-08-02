2e1ce51916a14e90f8994392ad1ea82b2e79a824
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:11:47 -0700

    updated CSS frameworks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
