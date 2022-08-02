1ce4b1f82208c586f7f7263c1505586f07a07787
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:10:30 -0700

    updated Portfolio page

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Portfolio = () => {
+    return (
+        <Container>
+            <div>THIS IS THE PORTFOLIO PAGE</div>
+        </Container>
+        )
+};
+
+export default Portfolio;
