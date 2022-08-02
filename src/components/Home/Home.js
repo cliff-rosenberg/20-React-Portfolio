c98650c9ab04b5ca4ed0562876d0a2d0255c7dc3
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:09:59 -0700

    updated Home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <Container>
+            <div>THIS IS THE HOME PAGE</div>
+        </Container>
+        )
+};
+
+export default Home;
