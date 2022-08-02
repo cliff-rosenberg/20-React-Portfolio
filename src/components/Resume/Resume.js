760d7e89e963897a9c8089fa5435c99d8b51ea91
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:10:41 -0700

    Updated Resume page

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Resume = () => {
+    return (
+        <Container>
+            <div>THIS IS THE RESUME PAGE</div>
+        </Container>
+        )
+};
+
+export default Resume;
