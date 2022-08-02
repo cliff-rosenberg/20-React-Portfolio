90f8885ae8e39134694760df050f2ae53d627e5c
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 15:09:23 -0700

    added router functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-import { MyNav } from './components/Navigation/MyNav'
+import MyNav from './components/Navigation/MyNav';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
+import Resume from './components/Resume/Resume';
 
 function App() {
   return (
     <div className="App">
-     <MyNav />
+    <Router>
+      <MyNav />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route exact path="/contact" element={<Contact />}></Route>
+        <Route exact path="/portfolio" element={<Portfolio />}></Route>
+        <Route exact path="/resume" element={<Resume />}></Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
