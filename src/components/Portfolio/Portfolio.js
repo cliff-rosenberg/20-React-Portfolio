256dd892b761efa7e5b9ff2c1e6f0ebe8015ab6d
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-02 18:28:13 -0700

    added text to Card

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,19 +11,21 @@ const Portfolio = () => {
         {title: "Card 1", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."},
         {title: "Card 2", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."},
         {title: "Card 3", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."},
-        {title: "Card 4", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."}
+        {title: "Card 4", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."},
+        {title: "Card 5", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."},
+        {title: "Card 6", text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."}
     ];
-    const cardQuan = 4;// set to amount of objects in array
+    const cardQuan = 6;// set to amount of objects in array
 
     useEffect(() => {
         runHolder('image-class-name');
     });
     return (
         <Container>
-            <Row xs={1} md={2} className="g-4">
+            <Row xs={1} md={3} className="g-4">
             {Array.from({ length: cardQuan }).map((_, idx) => (
                 <Col key={idx}>
-                    <Card>
+                    <Card className="portfolio-card">
                         <Card.Img variant="top" src="holder.js/320x240" />
                         <Card.Body>
                         <Card.Title>{cardsText[idx].title}</Card.Title>
