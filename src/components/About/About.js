6934167933ce0e0fb874177a78d634db1b424eea
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-07 01:27:11 -0700

    changed CSS library

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,35 +1,30 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import { MDBContainer, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage } from 'mdb-react-ui-kit';
 import './About.css'
 import cliff from '../../images/cliff-headshot2-sm.jpg';
 
 const About = () => {
     return (
-        <Container fluid="md" className="bkgrd">
-            <Row>
-                <Col> 
-                <Card border="dark" className="card-text">
-                    <Card.Body className="text-wide">
-                    <Card.Title>About Cliff</Card.Title>
-                    <Card.Text>
+        <MDBContainer breakpoint="md" className="vh-100 bkgrd">
+            <div className="d-flex justify-content-center flex-column flex-sm-row">
+
+                <MDBCard className="order-sm-2 card-picture mx-auto mx-sm-0">
+                    <MDBCardImage variant="top" className="img-fluid rounded" src={cliff} />
+                </MDBCard>
+                
+                <MDBCard border="dark" className="order-sm-1 card-text mx-auto mx-sm-0">
+                    <MDBCardBody>
+                    <MDBCardTitle>About Cliff</MDBCardTitle>
+                    <MDBCardText>
                     Cliff is an Automation Technician with more than a decade of experience deploying and optimizing interconnected manufacturing systems. 
                     Cliff has consistently delivered value through curating business-critical data and implementing energy, material, and time-saving code optimizations for Allen Bradley automation systems. 
                     In addition, Cliff is currently studying full-stack software development (HTML, CSS, Node, JavaScript, React, MongoDB) at The University of Arizona and is interested in future career challenges.
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                    <Card className="card-picture">
-                        <Card.Img variant="top" className="img-fluid" src={cliff} />
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-        )
+                    </MDBCardText>
+                    </MDBCardBody>
+                </MDBCard>
+            </div>
+        </MDBContainer>
+    )
 };
 
 export default About;
