bbeb840fa3051fbb56dda9ec4d96a9fc8ddbc842
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-07 18:59:22 -0700

    removed unneeded CSS library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './index.css';
 import App from './App';
