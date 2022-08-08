c912fd447f78209823c2d58f1332452c2aa6060e
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-07 18:58:34 -0700

    restyled CSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,12 @@ import hawaii from '../../images/cliff-hawaii.png'
 const Home = () => {
     return (
         <MDBContainer className="text-center vh-100 bkgrd">
-            <div>
+            <div className="pt-4">
                 <img src={hawaii} className='img-fluid rounded-pill shadow-4' alt='...' />
             </div>
-            <div className="fs-1">Welcome to my portfolio page!</div>
+            <div className="text-dark fs-1">Welcome to my portfolio page!</div>
         </MDBContainer>
-        )
+    )
 };
 
 export default Home;
