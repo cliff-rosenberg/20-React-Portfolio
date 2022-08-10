831e3eaba44211cda30c28a954a4ade2d238e6ec
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-09 18:31:35 -0700

    updated Resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { MDBContainer } from 'mdb-react-ui-kit';
+import { 
+    MDBContainer, 
+    MDBCard,
+    MDBCardImage,
+    MDBCardBody,
+    MDBCardTitle, 
+    MDBCardText,
+    MDBCardFooter,
+    MDBBtn
+ } from 'mdb-react-ui-kit';
+ import resumePic from '../../images/laptop-resume.jpg';
 
 const Resume = () => {
-    const pdfUrl = 'https://www.dropbox.com/s/9gg4m98v8e5vxhn/Cliff_Rosenberg_Resume.pdf?raw=1'
+    const pdfUrl = 'https://www.dropbox.com/s/9gg4m98v8e5vxhn/Cliff_Rosenberg_Resume.pdf?dl=0'
     return (
         <MDBContainer fluid className="vh-100">
-            {/* stored at "https://www.dropbox.com/s/9gg4m98v8e5vxhn/Cliff_Rosenberg_Resume.pdf?raw=1" */}
-            <object data={pdfUrl} type="application/pdf" width="100%" height="100%">
-                <iframe src={pdfUrl} title="Cliff Rosenberg Resume" width="100%" height="100%" style={{ border: "none" }}>
-                This browser does not support PDFs. Please download the PDF to view it: 
-                <a href={pdfUrl}>Download PDF</a>
-                </iframe>
-            </object>          
+            {/* stored at "https://www.dropbox.com/s/9gg4m98v8e5vxhn/Cliff_Rosenberg_Resume.pdf?dl=0" */}
+            <div className="d-flex justify-content-center">
+            <MDBCard className="w-25">
+                <MDBCardImage className="img-thumbnail" position="top" src={resumePic} />
+                <MDBCardBody>
+                <MDBCardTitle>Download Resume:</MDBCardTitle>
+                <MDBCardText>Press the button below to download a copy of my resume.</MDBCardText>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                <MDBBtn href={pdfUrl}>DOWNLOAD</MDBBtn>
+                </MDBCardFooter>
+            </MDBCard>
+            </div>
         </MDBContainer>
     )
 };
